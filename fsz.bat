d8e5049486a107297fec2a3855d74f6474ec2e2c
patientx <patient.x@gmail.com>
2025-02-07 13:42:39 +0300

    Add files via upload

---
+++ fsz.bat
@@ -0,0 +1,13 @@
+@echo off
+echo.
+echo *** Fish-Speech For AMD GPU's (Using ZLUDA) ***
+echo.
+set PYTHON="%~dp0/venv/Scripts/python.exe"
+set GIT=
+set VENV_DIR=./venv
+set ZLUDA_COMGR_LOG_LEVEL=1
+echo *** Checking and updating to new version if possible 
+:: git pull
+echo.
+.\zluda\zluda.exe -- %PYTHON% tools\run_webui.py --half
+pause
